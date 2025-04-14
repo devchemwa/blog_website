ca4a26ed539afb937bf5130abc2189ccc5a6508e
Chemwa <creatorjabatar@gmail.com>
2025-04-14 21:13:20 +0300

    admin page

---
+++ admin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+ <?php include 'header.php'; ?>
+<body>
+<?php include 'navbar.php'; ?>
+
+<div class="admin-panel">
+    <div class="admin-sidebar">
+        <aside>
+            <ul>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">New Blog Post</a></li>
+                <li><a href="#">Manage Blogs</a></li>
+            </ul>
+        </aside>
+        <div class="panel">
+            <?php  
+                        require 'config.php';
+                        $conn = mysqli_connect($server,$user, $pass, $db_name);
+                        if(!$conn){
+                            echo 'Connection Error: ' . mysqli_connect_error();
+                        }else{
+                        $sql = 'SELECT * FROM blog';
+                        $result = mysqli_query($conn,$sql);
+                        $blog = mysqli_fetch_assoc( $result ); 
+            ?>
+            <div class="admin-blog">
+                <p><?=$blog['title']; ?></p>
+                <p><?=$blog['author'];?></p>
+                <p><?=$blog['content'];?></p>
+                <p><?=$blog['created_at'];?></p>
+            </div>
+
+       <?php } ?>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
--- contact.php
+++ contact.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
- <?php include('header.php'); ?>
+ <?php include 'header.php'; ?>
 <body>
 <div class="container">
-  <?php include('navbar.php'); ?>
+  <?php include 'navbar.php'; ?>
   
   <div class="contact-form-container" id="contact-form-container">
-    <?php include('contact-form.php'); ?>
+    <?php include 'contact-form.php'; ?>
   </div>
 
   <div class="footer-container">
-  <?php include('footer.php'); ?>
+  <?php include 'footer.php'; ?>
   </div>
 </div>
 </body>
