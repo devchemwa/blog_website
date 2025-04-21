bad0f83e218b2cfe8eb89a437f86601e70fc60b6
Chemwa <creatorjabatar@gmail.com>
2025-04-21 22:40:16 +0300

    modified

--- user-verify.php
+++ user-verify.php
@@ -6,7 +6,7 @@ if(!$conn){
 }else{ 
 ?>
 <div>
-    <form action="user-verify.html.php">
+    <form action="user-verify.php" method="post">
     <input type="text" name="name" id="name" placeholder="name"><br><br>
     <input type="password" name="user_password" id="user_password" placeholder="password"><br><br>
     <input type="submit" name="verify-user" id="verify-user" placeholder="Login">
@@ -14,13 +14,17 @@ if(!$conn){
 </div>
 <?php 
          if(isset($_POST['verify-user'])){
-            $name = $_POST['name'];
-            $password = $_POST['password'];
-            $sql = mysqli_query($conn, "select name,user_password from users where name = '$name'");
-            $result = mysqli_fetch_all($sql);
+            $name = htmlspecialchars($_POST['name']);
+            $password = $_POST['user_password'];
+            $sql = mysqli_query($conn, "select name,user_password from users");
+            $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
             print_r($result);
+            for($i=0;$i < count($result); $i++){
+                    if  ($name == $result[$i]['name'] && $password == $result[$i]['user_password']){
+                        header('Location: http://localhost/blog_website/admin.php');
+                        die();
+                    }
+            }
          }
-
 ?>
-
 <?php } ?>
